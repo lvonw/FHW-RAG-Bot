5301d60ff9ab09062b1ebbc4fa0465aaa30a04db
Leopold von wendt <91526397+lvonw@users.noreply.github.com>
2023-11-10 13:11:46 +0100

    chroma init
    flag muss gesetzt sein wenn man chroma initialisieren möchte, sonst wird das gerade nur geladen um tokens zu sparen

--- main.py
+++ main.py
@@ -15,30 +15,51 @@ from langchain.vectorstores         import Chroma
 load_dotenv(find_dotenv())
 
 
-loader = PyPDFDirectoryLoader(path="data/pdfs/") 
-# docs = loader.load_and_split
-docs = loader.load()
 
-db = Chroma.from_documents(docs)
+
+# docs = loader.load()
+
 # Constants
-# GPT_TURBO   = "gpt-3.5-turbo"
-# TEMPERATURE = 0.3
-# API_KEY     = os.environ["OPENAI_API_KEY"]
-# LLM         = ChatOpenAI(
+GPT_TURBO   = "gpt-3.5-turbo"
+TEMPERATURE = 0.3
+API_KEY     = os.environ["OPENAI_API_KEY"]
+INIT_CHROMA = False
+
+# LLM         = ChatOpenAI(  
 #            model_name      = GPT_TURBO, 
 #            temperature     = TEMPERATURE, 
 #            openai_api_key  = API_KEY)
 
+def init_and_persist_chroma(docs): 
+    db = Chroma.from_documents(
+        docs, 
+        OpenAIEmbeddings(openai_api_key=API_KEY),
+        persist_directory   = "./data/vectordb/")
+    return db
+
+
+def load_chroma():
+    return Chroma(
+        embedding_function  = OpenAIEmbeddings(openai_api_key=API_KEY),
+        persist_directory   = "./chroma_db") 
+        
 
 # ============ MAIN ============
 def get_user_input():
     return input("Please enter the fact you want to get verified: ")
 
+
+
 def main():
-    print(len(docs))
-    for d in docs:
-        print (d.metadata)
-    print(db)
+    db = None
+    if INIT_CHROMA:
+        loader      = PyPDFDirectoryLoader(path="data/pdfs/") 
+        splitter    = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        docs        = loader.load_and_split(text_splitter=splitter)
+        db          = init_and_persist_chroma(docs)
+    else:
+        db = load_chroma()
+    # print(db)
 
 if __name__ == "__main__":
     main()
