f5fd8ce21dc7eae8eb6c92668149001b8fc8a306
Leopold von wendt <91526397+lvonw@users.noreply.github.com>
2023-11-15 14:07:56 +0100

    main aufgeräumt

---
+++ constants.py
@@ -0,0 +1,30 @@
+import os
+from dotenv                         import load_dotenv, find_dotenv
+from enum                           import Enum
+from langchain.chat_models          import ChatOpenAI
+from langchain.prompts              import PromptTemplate
+
+load_dotenv(find_dotenv())
+
+PATH_VECTORDB   = "./data/vectordb/"
+PATH_PDF        = "data/pdfs/"
+
+INPUT_PROMPT    = "Wie kann ich Dir helfen?: "
+
+GPT_TURBO       = "gpt-3.5-turbo"
+TEMPERATURE     = 0.0
+API_KEY         = os.environ["OPENAI_API_KEY"]
+LLM             = ChatOpenAI(  
+    model_name      = GPT_TURBO, 
+    temperature     = TEMPERATURE, 
+    openai_api_key  = API_KEY)
+
+TEMPLATE        = """ 
+Beantworte die Frage ausschließlich mit dem hier gegebenen Kontext:
+{context}
+
+Frage: {question}""" 
+
+PROMPT = PromptTemplate(
+    input_variables = ["context", "question"],
+    template        = TEMPLATE)
--- main.py
+++ main.py
@@ -1,74 +1,47 @@
 import os
+import argparse
+import constants
 
-from dotenv                         import load_dotenv, find_dotenv
-
-from langchain.chat_models          import ChatOpenAI
-from langchain.prompts              import PromptTemplate
 from langchain.schema               import StrOutputParser
-
+from langchain.schema.runnable      import RunnablePassthrough
 from langchain.document_loaders     import PyPDFDirectoryLoader
 from langchain.embeddings.openai    import OpenAIEmbeddings
 from langchain.text_splitter        import CharacterTextSplitter
 from langchain.vectorstores         import Chroma
-from langchain.schema.runnable      import RunnablePassthrough
 
-
-# Load the environment variables. 
-load_dotenv(find_dotenv())
-
-
-# Constants
-GPT_TURBO   = "gpt-3.5-turbo"
-TEMPERATURE = 0.0
-API_KEY     = os.environ["OPENAI_API_KEY"]
 INIT_CHROMA = False
 
-LLM         = ChatOpenAI(  
-            model_name      = GPT_TURBO, 
-            temperature     = TEMPERATURE, 
-            openai_api_key  = API_KEY)
-
+# ============ MAIN ============
+def ask_question(question, retriever):
+    question_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()} 
+        | constants.PROMPT 
+        | constants.LLM 
+        | StrOutputParser())
+    return question_chain.invoke(question)
+    
 def init_and_persist_chroma(docs): 
     db = Chroma.from_documents(
         docs, 
-        OpenAIEmbeddings(openai_api_key=API_KEY),
-        persist_directory   = "./data/vectordb/")
+        OpenAIEmbeddings(openai_api_key=constants.API_KEY),
+        persist_directory   = constants.PATH_VECTORDB)
     return db
-
 
 def load_chroma():
     return Chroma(
-        embedding_function  = OpenAIEmbeddings(openai_api_key=API_KEY),
-        persist_directory   = "./data/vectordb/") 
+        embedding_function  = OpenAIEmbeddings(openai_api_key=constants.API_KEY),
+        persist_directory   = constants.PATH_VECTORDB) 
         
-
-# ============ MAIN ============
 def get_user_input():
-    return input("Please enter the fact you want to get verified: ")
-
-template = """ 
-Beantworte die Frage ausschließlich mit dem hier gegebenen Kontext:
-{context}
-
-Frage: {question}
-""" 
-# Prompts
-prompt = PromptTemplate(
-    input_variables = ["context", "question"],
-    template        = template
-)
-
-
-
+    return input(constants.INPUT_PROMPT)
 
 def format_docs(docs):
     return "\n\n".join([d.page_content for d in docs])
-    #return docs[0].page_content
 
 def main():
     db = None
     if INIT_CHROMA:
-        loader      = PyPDFDirectoryLoader(path="data/pdfs/") 
+        loader      = PyPDFDirectoryLoader(path=constants.PATH_PDF) 
         splitter    = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         docs        = loader.load_and_split(text_splitter=splitter)
         db          = init_and_persist_chroma(docs)
@@ -76,34 +49,12 @@ def main():
         db = load_chroma()
     
     retriever = db.as_retriever()
-    question1 = "Was kannst du mir zum Thema Freiwilliges auslandssemster sagen"
-    question2 = "Welche modul nr hat Grundlagen der Computergrafik?"
 
-    #print(retriever.invoke(question)[0].page_content)
-    chain1 = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()} 
-        | prompt 
-        | LLM 
-        | StrOutputParser())
+    user_input = get_user_input()
 
-    # result = chain1.invoke(question1)
-    # print(result)
-    # print("===================")
-    # result = chain1.invoke(question2)
-    # print(result)
-    # print("===================")
 
-    print(format_docs(retriever.invoke(question2)))
-    #print(format_docs(retriever.invoke(question1)))
-    # print(format_docs(retriever.invoke(question1)))
-    result = chain1.invoke(question2)
-    print(result)
+    print(ask_question(user_input, retriever))
     print("===================")
-
-
-    # print(len(retrieved_docs))
-    # print(retrieved_docs[0].page_content)
-
 
 
 if __name__ == "__main__":
