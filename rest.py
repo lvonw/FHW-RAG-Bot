8b908e34219418d7027cdcd5a9255e1f8b40004f
merte <inf103461@gmail.com>
2023-11-23 22:08:51 +0100

    Fixed error handling in rest.py

--- rest.py
+++ rest.py
@@ -11,17 +11,34 @@ def query():
     try:
         data = request.get_json()
 
-        if 'prompt' not in data:
+        if 'prompt' in data and data['prompt'] == "":
+            return jsonify({'error': 'Empty prompt'}), 400
+        
+        if 'prompt' not in data and ('init' not in data or 'init' in data and not data['init']):
             return jsonify({'error': 'Missing "prompt" element in request body'}), 400
 
+        if 'prompt' in data and 'init' in data and data['init']:
+            return jsonify({'error': 'Cannot initialize and respond to prompt at the same time'}), 400
+
         args = constants.DefaultArgs
-        args.question = data['prompt']
+        if 'prompt' in data:
+            args.question = data['prompt']
+        if 'database' in data:
+            args.database = data['database']
+        if 'model' in data:
+            args.model = data['model']
+        if 'init' in data:
+            args.init = data['init']
+        if 'cv' in data:
+            args.cv = data['cv']
+        
         
         result = main.invoke_chain(args)
 
         return jsonify({'response': result})
-    
+
     except Exception as e:
+        # print(e)
         return jsonify({'error': str(e)}), 500
     
 if __name__ == '__main__':
--- test/test_main.py
+++ test/test_main.py
@@ -2,8 +2,8 @@ import main
 import constants
 
 
-def test_invoke_chain_rest():
-    args = constants.DefaultArgs
-    args.question = "What's 2+2?"
-    result = main.invoke_chain(args)
-    print(result)
+# def test_invoke_chain_rest():
+#     args = constants.DefaultArgs
+#     args.question = "What's 2+2?"
+#     result = main.invoke_chain(args)
+#     print(result)
